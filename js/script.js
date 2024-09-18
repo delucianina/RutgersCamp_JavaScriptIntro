eb1ea8eddc68565723f1bb239d577316f15edfc3
delucianina <delucianina@gmail.com>
2024-09-18 13:50:35 -0400

    for loop basics

--- js/script.js
+++ js/script.js
@@ -2,28 +2,36 @@
 // REFERENCE TYPE  
 // They store a list of values 
 
-// const fruits = ['apple', 'orange', 'kiwi', 'grape'];
-// console.log(fruits);
-// console.log(fruits.length);
+// ONLY TWO VALUE TYPES THAT CAN BE ITERATED OVER: ARRAYS AND STRINGS 
 
-// fruits[2] = 'strawberry'
-
-// console.log(fruits);
+const fruits = ['apple', 'orange', 'kiwi', 'grape', 'strawberry', 'lychee', 'plantain'];
+const names = ['Roman', 'Michael', 'Nina', 'Adonis'];
+const data = [
+    ['Nina', 'DeLucia', 31, '1234 Market Street', '555-555-5555'], 
+    ['baking', 'jogging']
+];
 
 /*
-// EXAMPLE - THOUGH THIS IS NOT THE BEST WAY TO HANDLE USER INFORMATION (USE OBJECT INSTEAD)
-// NEST ARRAYS AND CALL ON THEM LIKE THIS
-const data = [['Nina', 'DeLucia', 31, '1234 Market Street', '555-555-5555'], ['baking', 'jogging']];
-console.log(data[1][1]); 
-
-// INCREASE DATA 
-data[2]++; 
-console.log(data[2]); 
-// ADD TO A STRING
-let fullName = "Nina"
-fullName += ' DeLucia';
-console.log(fullName);
+// BASIC FOR LOOP MADE OF 3 PARTS:
+// ASSIGNMENT OF VARIABLE 
+// CONDITION - LOOP REPEATS WHILE THIS IS TRUE 
+// INCREMENTOR 
+// HERE IS AN EXAMPLE OF A SIMPLE LOOP 
+for (let count = 1; count <= 6; count++) {
+    console.log(count);
+}
+// LOOPS NEED TO FINISH BEFORE THE REST OF YOUR CODE RUNS
+console.log('The loop is done running!');
 */
 
+// "For now, I'm going to leave you with this one loop that you can use for any of your looping needs"
 
-// LOOP 
+/*
+// USE FOR LOOPS TO PRINT OUT THE ITEMS IN AN ARRAY 
+// DO IT HERE WITH FRUITS ARRAY ABOVE:
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+// i IS FOR INDEX
+*/
+
