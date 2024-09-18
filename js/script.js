c6ef68d24b744e58c8eeab794028d4010c294057
delucianina <delucianina@gmail.com>
2024-09-18 13:03:53 -0400

    finished array basics

--- js/script.js
+++ js/script.js
@@ -2,11 +2,28 @@
 // REFERENCE TYPE  
 // They store a list of values 
 
-const fruits = ['apple', 'orange', 'kiwi', 'grape'];
-console.log(fruits);
-console.log(fruits.length);
+// const fruits = ['apple', 'orange', 'kiwi', 'grape'];
+// console.log(fruits);
+// console.log(fruits.length);
 
+// fruits[2] = 'strawberry'
 
+// console.log(fruits);
+
+/*
 // EXAMPLE - THOUGH THIS IS NOT THE BEST WAY TO HANDLE USER INFORMATION (USE OBJECT INSTEAD)
-const data = ['Nina', 'DeLucia', 31, '1234 Market Street', '555-555-5555'];
-console.log(data[3]);
+// NEST ARRAYS AND CALL ON THEM LIKE THIS
+const data = [['Nina', 'DeLucia', 31, '1234 Market Street', '555-555-5555'], ['baking', 'jogging']];
+console.log(data[1][1]); 
+
+// INCREASE DATA 
+data[2]++; 
+console.log(data[2]); 
+// ADD TO A STRING
+let fullName = "Nina"
+fullName += ' DeLucia';
+console.log(fullName);
+*/
+
+
+// LOOP 
