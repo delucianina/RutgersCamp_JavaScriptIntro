2115a9896aa6f768e0be2b7b370c7a39f02054d3
delucianina <delucianina@gmail.com>
2024-09-18 11:55:40 -0400

    finish conditional overvier

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 let firstName = "Nina";
 let age = 5;
 
-age %= 2;
-console.log(age);
+//  If age is greater than 20 and less than 50, console log 'you are young' 
 
-console.log('test');
+//  If age is greater than or equal to 50, console log 'you are getting up there' 
+
+//  If age is less than 20 and greater than 12, console log 'you are a teenager' 
+
+if (age > 12 && age < 20) {
+    console.log("You are a teenager.");
+} else if (age >= 20 && age < 50) {
+    console.log("You are young.");
+} else if (age >= 50) {
+    console.log("You are getting up there...");
+} else {
+    console.log("Age is outside the program's range.");
+} 
+
+
+const result = 10 || "x" || false || 'some string';
+console.log(result);
+
+const result2 = 0 || 75 && true && 'some string' && true && 100;
+console.log(result2);
+
+const result3 = 0 || 75 || true && 'some string' && true && 100;
+console.log(result3);
