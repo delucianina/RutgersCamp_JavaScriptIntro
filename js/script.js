1dc4b99c43bc1576f880fc366ac140f116c857c7
delucianina <delucianina@gmail.com>
2024-09-19 12:38:59 -0400

    finished functions overview

--- js/script.js
+++ js/script.js
@@ -28,3 +28,25 @@ printArray();
 // SCOPE - LEVELS OF MEMORY (i.e. GLOBAL SCOPE; DECLARING A VARIABLE IN A FUNCTION MEANS IT IS 'SCOPED')
 // LEAVING OUT THE 'LET' IN THE DECLARATION OF A FOR LOOP WILL MAKE THE VARIABLE GLOBAL. THIS IS BAD 99% OF THE TIME 
 */ 
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return num1 + num2;
+}
+
+add(10, 5);
+add(20, 100);
+add(50, 5);
+
+function printVals(val1, val2, val3, val4) {
+    console.log(val4[1]);
+}
+
+printVals('some string text', true, 15, ['Michael', 'Nina', 'Roman']);
+
+
+// WHEN YOU CALL UPON A FUNCTION, IT CAN ANSWER BACK
+// BY DEFAULT, IT ANSWERS BACK UNDEFINED 
+const variableResult = add();
+console.log(variableResult);
+// USE THE RETURN KEYWORD IN a FUNCTION TO CONTROL WHAT IT ANSWERS BACK WITH 
