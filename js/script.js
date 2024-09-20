68e3de2249f6c7be7e25f2b598ef6fe2c62363e0
delucianina <delucianina@gmail.com>
2024-09-20 14:30:45 -0400

    API basics

--- js/script.js
+++ js/script.js
@@ -1,102 +1,95 @@
-// -------------------------------------
-// LEARN BASICS OF OBJECTS 
-// -------------------------------------
-// -------------------------------------
-// AN OBJECT IS AN "ORGANIZED DATA TYPE"
-// THE SYNTAX OF USING THE DOT + METHOD IS CALLED 'DOT NOTATION' 
-/*
-// THIS IS AN EMPTY OBJECT 
-const user = {
+// ------------------------------------
+// ------------------------------------
+// LEARNING APIs
+// ------------------------------------
+// THIS ARGUMENT TAKES A STRING - CAN BE ANY AND ALL CSS SELECTORS 
+const header = document.querySelector('h1');
+const paragraph = document.querySelector('p:nth-child(2');
+// const image1 = document.querySelector('#main-image');
+const noteOutput = document.querySelector('.output');
+const timerDisplay = document.querySelector('#time');
+const timerBtn = document.querySelector('#start-btn');
+// ------------------------------------
+// ------------------------------------
+// ADD A NEW CSS CLASS TO AN ELEMENT IN HTML 
+// ------------------------------------
+// ADD A NEW CLASS TO AN ELEMENT 
+header.classList.add('crazy');
+// APPLY A NEW STYLE DIRECTLY TO AN ELEMENT INSTEAD OF USING CSS
+// MUST USE CAMELCASE - CAN'T HAVE '-'
+header.style.textDecoration = 'underline';
+// ------------------------------------
+// ------------------------------------
+// GRAB AND OUTPUT THE ELEMENTS OF HTML
+// ------------------------------------
+// WE USED TO DO THIS A GOOD 20 YEARS AGO, NOW USE QUERY SELECTOR INSTEAD OF DOING THIS  
+// console.log(document.children[0].children[1]);
 
-}; // INCLUDE THE ; HERE!
+// image1.setAttribute('src', 'https://images.pexels.com/photos/28403277/pexels-photo-28403277/free-photo-of-almere-central-station.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+
+// ------------------------------------
+// ------------------------------------
+// INSERTING A NEW ARTICLE ELEMENT INTO THE OUTPUT SELECTOR 
+// ------------------------------------
+// ADJACENT HTML COULD BE IN FRONT, BEHIND, INSIDE... 
+// noteOutput.insertAdjacentHTML('beforeend', '<article><h1>some header</h1></article>');
+// OR, INSTEAD OF QUOTATION STRING, WE CAN USE BACKTICKS - [ ` ] - TO CREATE A TEMPLATE LITERAL (or TEMPLATE STRING)  
+noteOutput.insertAdjacentHTML('beforeend', `
+    <article>
+        <h3>New note text</h3>
+        <p>Added On: 9/20/2024</p>
+    </article>
+`);
+
+// ------------------------------------
+// ------------------------------------
+// TIMERS
+// ------------------------------------
+// FOR THINGS LIKE QUIZES, COUNTDOWNS, ETC. MORE COMMON THAN YOU'D THINK
+// [window.] grabs the browser 
+// PASSING A FUNCTION AS THE ARGUMENT - THE FUNCTION SITS AND WAITS FOR THE TIMER TO COUNT DOWN BEFORE IT RUNS 
+// THEN WE SET TIME IN MS (that is 3 seconds)
+/*
+window.setTimeout(function() {
+    console.log('Time is UP!');
+}, 3000);
 */
-// -------------------------------------
-// -------------------------------------
-// LET'S STORE USER INFO
-// SYNTAX IS SIMILAR TO CSS, BUT NOT THE SAME. i.e. - SEPARATE PROPERTIES/VALUES WITH , NOT ;
-const user = {
-    name: 'Nina',
-    age: 31,
-    phone: '555-555-5555',
-    address: '1234 Market Street',
-    info: {
-        location: 'Philly',
-        hobbies: ['jogging', 'baking'],
-    },
-    printAge: function() {
-        console.log(this.age); //'this' keyword points to parent object
-    },
-    haveBirthday: function() {
-        this.age++;
-        console.log('Happy Birthday! You are now', this.age, 'years old!');
-    },
-    addHobby: function(newHobby) {
-        // Need to reference an argument that is passed to addHobby (string of a hobby)
-        // Push the argunent(hobby) to the info.hobbies array
-        this.info.hobbies.push(newHobby);
-        // this is saving it
-    },
-    printHobby: function() {
-        //console.log the hobbies array 
-        // console.log(this.info.hobbies[2]);
-        // BONUS - loop over the hobbies array and console.log that 
-        // In JavaScript, the standard for loop is fastest (with the 3 parts) 
-        for (let i = 0; i < this.info.hobbies.length; i++) {
-            console.log(this.info.hobbies[i]);
-        }
-        /*
-        // YOU CAN MAKE A SIMPLER LOOP WITH THE OF KEYWORD - 'OF' POINTS AT THE STRING AND PRINTS AS LONG AS IT IS TRUE (EXISTS)
-        for (let hobby of this.info.hobbies) {
-            console.log(hobby);
-        }
-            */
-    },
-    emptyFunction: function() {
-        //Empty
+// THIS IS CALLED A "CALLBACK" or "CALLBACK FUNCTION". WE DON'T NAME IT BECAUSE BEHIND THE SCENES THE FUNCTION HAS A PARAMETER (IN THE API WE'RE USING) 
+let count = 10;
+// USE BOOLEAN TO RUN TIMER ONLY ONCE 
+let started = false;
+// REPEAT LIKE THIS. THIS ONE STOPS AT 0
+/*
+const timer = setInterval(function() {
+    count--;
+
+    timerDisplay.innerText = 'Time: ' + count;
+
+    if (count <= 0) {
+        clearInterval(timer);
     }
-};
-// console.log()
-user.addHobby('gaming');
-user.printHobby(); 
-// RUNNING A FUNCTION BY NAME WITHOUT CALLING IT WILL RETURN THE ENTIRE FUNCTION 
-// console.log(user.emptyFunction);
-/*
-// PRINT THE AGE
-console.log(user.age);
-// PRINT THE ADDRESS 
-console.log(user.address);
-// CAN ALSO JUST PRINT ONE INDEXED ITEM OF STRING
-console.log(user.address[8]);
+}, 1000);
+// CLEAR INTERVAL STOPS THE TIMER
 */
-/* 
-// THIS ERROR KILLS THE PROGRAM!
-// IF YOU TRY TO DOT NOTATE SOMETHING THAT IS NOT AN OBJECT, THE ERROR WILL SAY IT CANNOT READ THE PROPERTIES. BUT UNFORTUNATELY IT WON'T SPECIFY THAT YOU AREN'T CALLING ON AN OBJECT
-// console.log(null.length); 
-*/
-/*
-// ALSO THROWS ERROR THAT IT CANNOT READ THE PROPERTY
-console.log(user.height);
-// IF YOU'RE GIVEN A FALSEY RESPONSE, YOU CAN USE THAT TO WRITE CONDITIONAL CODE - LIKE "YOU HAVE NOT PROVIDED THIS INFO; PLEASE GIVE IT TO US NOW"
-if (!user.height) {
-    console.log('You have not provided this info');
-}
-*/
-/*
-// ACCESSING NESTED DATA
-console.log(user.info.hobbies[1]);
-*/
-/*
-// PULL INFO FROM A STAR WARS API - https://swapi.dev/ - AND PRINT IT OUT TO THE CONSOLE 
-fetch('https://swapi.dev/api/people')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-    })
-*/
-// CALLING A FUNCTION STORED INSIDE AN OBJECT 
-// user.haveBirthday();
 
+// EVENT LISTENER TAKES 2 TYPES OF INPUT 
+timerBtn.addEventListener('click', function() {
+    if (!started) {
+        const timer = setInterval(function() {
+            count--;
+        
+            timerDisplay.innerText = 'Time: ' + count;
+        
+            if (count <= 0) {
+                clearInterval(timer);
 
+                timerDisplay.innerText = "Time: 10";
+                count = 10;
+                started = false;
+            }
+        }, 1000);
 
-// -------------------------------------
-// -------------------------------------
+        started = true;
+    }
+});
+// BE CAREFUL, YOU CAN STACK INTERVALS AND RUN MULTIPLE INTERVALS AT ONCE 
